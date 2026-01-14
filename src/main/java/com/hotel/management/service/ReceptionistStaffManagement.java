50ff84110b96064af26c5b10faa6f27e7515f5dc
DasunTharu <dasun.da.tharaka@gmail.com>
2026-01-14 14:02:00 +0530

    ReceptionistS
    ReceptionistStaffManagement

---
+++ src/main/java/com/hotel/management/service/ReceptionistStaffManagement.java
@@ -0,0 +1,28 @@
+package com.hotel.management.service;
+
+import com.hotel.management.model.ReceptionistStaff;
+import java.util.ArrayList;
+
+public class ReceptionistStaffManagement {
+
+    private ArrayList<ReceptionistStaff> receptionistList = new ArrayList<>();
+
+    public void addReceptionist(ReceptionistStaff r) {
+        receptionistList.add(r);
+    }
+
+    public boolean removeReceptionist(String id) {
+        return receptionistList.removeIf(r -> r.getId().equals(id));
+    }
+
+    public ArrayList<ReceptionistStaff> getAllReceptionists() {
+        return receptionistList;
+    }
+
+    public ReceptionistStaff findReceptionist(String id) {
+        for (ReceptionistStaff r : receptionistList) {
+            if (r.getId().equals(id)) return r;
+        }
+        return null;
+    }
+}
